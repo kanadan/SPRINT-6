fd8fa09569a7db29d13d983aaadcc708b9f495c1
Daniela Kanan <danykanan@192.168.1.9>
2024-09-25 16:18:36 -0600

    Update file path for vehicles_us.csv

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('/Users/danykanan/Software_Project/vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
 st.title('Car Sales Data Analysis') 
 st.header('Exploratory Data Analysis of Car Ads')  
